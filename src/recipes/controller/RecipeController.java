9fd1b39fe3e857850ba9aa1a9710a29a3231070f
beginner1986 <beginner1986@gmail.com>
2021-09-28 22:31:35 +0200

    Data model and simple REST controller added.

---
+++ src/recipes/controller/RecipeController.java
@@ -0,0 +1,22 @@
+package recipes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import recipes.model.Recipe;
+
+@RestController
+public class RecipeController {
+    Recipe recipe;
+
+    @PostMapping("/api/recipe")
+    public void postRecipe(@RequestBody Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    @GetMapping("/api/recipe")
+    public Recipe getRecipe() {
+        return this.recipe;
+    }
+}
