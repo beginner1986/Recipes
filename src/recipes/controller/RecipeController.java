ea464acafe8ae5db758aee66db63ebcaaa7a1bb5
beginner1986 <beginner1986@gmail.com>
2021-10-06 22:07:37 +0200

    Multiple recipes storage implemented. Posting new recipes implemented.

--- src/recipes/controller/RecipeController.java
+++ src/recipes/controller/RecipeController.java
@@ -1,22 +1,35 @@
 package recipes.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import recipes.model.Recipe;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @RestController
 public class RecipeController {
-    Recipe recipe;
+    private ConcurrentMap<Long, Recipe> recipes = new ConcurrentHashMap<>();
 
-    @PostMapping("/api/recipe")
-    public void postRecipe(@RequestBody Recipe recipe) {
-        this.recipe = recipe;
+    @PostMapping("/api/recipe/new")
+    public Map postRecipe(@RequestBody Recipe recipe) {
+        long id = recipes.size() + 1;
+        recipes.put(id, recipe);
+
+        return Collections.singletonMap("id", id);
     }
 
-    @GetMapping("/api/recipe")
-    public Recipe getRecipe() {
-        return this.recipe;
+    @GetMapping("/api/recipe/{id}")
+    public Recipe getRecipe(@PathVariable long id) {
+        Recipe recipe = recipes.get(id);
+
+        if(recipe != null) {
+            return recipe;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
